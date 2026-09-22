90187aa1a4205fc3eb88b84ca5505797ca6e6a19
Chan <t54292768@gmail.com>
2026-09-22 22:56:00 +0900

    week03 README.md file upload

---
+++ Frontend/React/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import OnboardingFlow from './component/OnboardingFlow';
+
+function App() {
+  return (
+    <OnboardingFlow/>
+  );
+}
+
+export default App;
